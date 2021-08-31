779dde42f1ee0e4558ac9783f90afcd484e3689f
Luis Vinicius M <luisvinicius0906@gmail.com>
2021-08-31 19:00:12 -0300

    Add logic to test redux store

--- app/screens/Main.tsx
+++ app/screens/Main.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
 import { COLORS, FONTS, SIZES } from "../constants";
-// import { icons } from "../constants";
+import usersActions from "../config/actions";
+import { useDispatch, useSelector } from "react-redux";
 
 const Main = (): JSX.Element => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  const increment = () => {
+    dispatch(usersActions.incrementValue);
+  }
+
+  const decrement = () => {
+    dispatch(usersActions.decrementValue);
+  }
+
   return (
     <View style={styles.container}>
       <View>
         <Text>Test Main Screen</Text>
+        <Text>COUNT: {state.value}</Text>
+        <TouchableOpacity onPress={() => increment()  }>
+          <Text>
+            CLICK ME TO INCREMENT
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={decrement}>
+          <Text>
+            CLICK ME TO DECREMENT
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
