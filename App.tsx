a05f99f9db1fdf11626cf12a4a05ff34ca380253
Luis Vinicius <luisvinicius0906@gmail.com>
2021-08-31 11:09:14 -0300

    Add App file structure to handle navigation container and loading fonts

--- App.tsx
+++ App.tsx
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+import * as Font from "expo-font";
+// import AppLoading from 'expo-app-loading';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "Roboto-Bold": require("./app/assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Black": require("./app/assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Regular": require("./app/assets/fonts/Roboto-Regular.ttf"),
+  });
+};
+
+// Screens
+
+//Tabs
+import Tabs from "./app/navigation/tabs";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  const [loaded, setLoaded] = React.useState(false);
+
+  // if (!loaded) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={fetchFonts}
+  //       onFinish={() => setLoaded(true)}
+  //       onError={console.warn}
+  //     />
+  //   )
+  // }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName={"Main"}
+      >
+        {/* Tabs */}
+        <Stack.Screen name="Tabs" component={Tabs} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        {/* Screens */}
+        {/* <Stack.Screen name="" component={} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default () => {
+  return <App />;
+};
