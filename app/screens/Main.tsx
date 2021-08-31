f9bfc156c92d8712a620c23db056b066b9d89020
Luis Vinicius M <luisvinicius0906@gmail.com>
2021-08-31 18:18:56 -0300

    Remove useless code of main screen

--- app/screens/Main.tsx
+++ app/screens/Main.tsx
@@ -6,39 +6,9 @@ import { COLORS, FONTS, SIZES } from "../constants";
 const Main = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          height: "30%",
-          backgroundColor: COLORS.blue,
-          paddingLeft: SIZES.padding,
-          paddingRight: SIZES.padding,
-          paddingTop: SIZES.padding + 20,
-        }}
-      >
-        <View style={[styles.flexRowCenter, styles.headerTopContainer]}>
-          <View>
-            <TouchableOpacity>
-              {/* <Image source={icons.menu} /> */}
-            </TouchableOpacity>
-          </View>
-          <View>
-            <View style={styles.flexRowCenter}>
-              <Text style={{ color: COLORS.white }}>Olá, </Text>
-              <Text style={[FONTS.h3, { color: COLORS.white }]}>
-                Luis Vinicius
-                {/* TODO: Handle big names (Luis Vinicius M...) or Just get the first two names*/}
-              </Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.headerSellingsToday}>
-          <Text style={[FONTS.h3, { color: COLORS.white }]}>Vendas hoje</Text>
-          <Text style={[FONTS.h1, { color: COLORS.white }]}>R$ 8,420,00</Text>
-        </View>
+      <View>
+        <Text>Test Main Screen</Text>
       </View>
-
-      <View></View>
     </View>
   );
 };
@@ -51,12 +21,8 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-  },
-  headerTopContainer: {
-    justifyContent: "space-between",
-  },
-  headerSellingsToday: {
-    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
